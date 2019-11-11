d86f4a37a4e6cc1520da965b323944270929a6b8
Tyler Cone <herrflockig@gmail.com>
2019-11-11 00:12:21 -0500

    Adding setup files
    Added requirements_dev.txt for developers on this project and a setup.py for installing and deploying.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="arrange-honeycomb",
+    version="0.0.1",
+    author="Tyler Cone",
+    author_email="herrflockig@gmail.com",
+    description="A command line tool to arrange icons for the Honeycomb plugin for Rainmeter.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arrange-honeycomb/arrange-honeycomb/",
+    packages=find_packages(),
+    install_requires=['hexy', 'numpy'],
+    entry_points={
+        'console_scripts': [
+            'arrange-honeycomb = arrangehoneycomb.__main__:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
